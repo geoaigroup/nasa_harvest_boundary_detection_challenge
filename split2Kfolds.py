f3c930c5dfd460ab88a0f0e6cf0f1ae8d795be2f
Hasan Nasrallah <50121708+hasan-nn@users.noreply.github.com>
2023-02-28 01:47:07 +0200

    Update split2Kfolds.py

--- split2Kfolds.py
+++ split2Kfolds.py
@@ -28,7 +28,7 @@ def main():
     df = pd.DataFrame(df)
     print(df)
     print(df['fold'].value_counts())
-    df.to_csv('folds5.csv',index=False)
+    df.to_csv('folds.csv',index=False)
 
 if __name__ =='__main__':
     main()
